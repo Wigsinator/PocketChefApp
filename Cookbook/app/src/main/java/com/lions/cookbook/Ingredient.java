bf28db80bb770cd947aee2cadddd560601b06ea0
Jennifer Huynh <jenniferr.huynh@mail.utoronto.ca>
2020-02-11 18:27:18 -0500

    add Recipe and Ingredient class

---
+++ Cookbook/app/src/main/java/com/lions/cookbook/Ingredient.java
@@ -0,0 +1,37 @@
+package com.lions.cookbook;
+
+public class Ingredient {
+    private String name;
+    private float quantity;
+    private String quantity_type;
+
+    public Ingredient(String name, float quantity, String quantity_type){
+        this.name = name;
+        this.quantity = quantity;
+        this.quantity_type = quantity_type;
+    }
+
+    public void changeName(String name){
+        this.name = name;
+    }
+
+    public void changeQuantity(float quantity){
+        this.quantity = quantity;
+    }
+
+    public void changeQuantityType(String quantity_type){
+        this.quantity_type = quantity_type;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public float getQuantity(){
+        return this.quantity;
+    }
+
+    public String getQuantityType(){
+        return this.quantity_type;
+    }
+}
