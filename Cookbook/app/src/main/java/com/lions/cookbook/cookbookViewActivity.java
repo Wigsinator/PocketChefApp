347a13fa054b333875116c09573e56212ad206c1
Harsh Gohil <harsh.gohil@mail.utoronto.ca>
2020-02-12 20:06:53 -0500

    adding cookbook view to branch 31

---
+++ Cookbook/app/src/main/java/com/lions/cookbook/cookbookViewActivity.java
@@ -0,0 +1,14 @@
+package com.lions.cookbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cookbookViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cookbook_view);
+    }
+}
