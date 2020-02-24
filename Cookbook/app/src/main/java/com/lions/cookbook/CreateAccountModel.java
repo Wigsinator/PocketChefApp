8b43f70157861938f4cac50ec72debd1526eb797
Jennifer Huynh <jenniferr.huynh@mail.utoronto.ca>
2020-02-23 23:43:49 -0500

    [LION-62] Add CreateAccountModel

---
+++ Cookbook/app/src/main/java/com/lions/cookbook/CreateAccountModel.java
@@ -0,0 +1,18 @@
+package com.lions.cookbook;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.Task;
+
+public class CreateAccountModel {
+    private FirebaseAuth mAuth;
+
+    public CreateAccountModel(){
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public boolean AddNewUser(String email,String userPassword){
+        Task<AuthResult> addUserResult = mAuth.createUserWithEmailAndPassword(email, userPassword);
+        //if successful returns true; if email already in use then return false
+        return addUserResult.isSuccessful();
+    }
+}
