8ce661a2d40a0d0ffd403893203ce8cfaaea6243
Jennifer Huynh <jenniferr.huynh@mail.utoronto.ca>
2020-02-25 15:05:19 -0500

    [LION-24] Edit CreateAccountContract

--- Cookbook/app/src/main/java/com/lions/cookbook/CreateAccountModel.java
+++ Cookbook/app/src/main/java/com/lions/cookbook/CreateAccountModel.java
@@ -3,14 +3,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.AuthResult;
 import com.google.android.gms.tasks.Task;
 
-public class CreateAccountModel {
-    private FirebaseAuth mAuth;
+public class CreateAccountModel implements CreateAccountContract.CreateAccountModel {
 
-    public CreateAccountModel(){
-        mAuth = FirebaseAuth.getInstance();
-    }
+    public CreateAccountModel(){}
 
-    public boolean AddNewUser(String email,String userPassword){
+    public Boolean addNewUser(String email,String userPassword){
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
         Task<AuthResult> addUserResult = mAuth.createUserWithEmailAndPassword(email, userPassword);
         //if successful returns true; if email already in use then return false
         return addUserResult.isSuccessful();
