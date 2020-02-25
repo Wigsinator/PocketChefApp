8583658e961eaf46670ade84492b2c3079e9b7e6
Jennifer Huynh <jenniferr.huynh@mail.utoronto.ca>
2020-02-25 13:25:45 -0500

    [LION-65] Create LogInModel

---
+++ Cookbook/app/src/main/java/com/lions/cookbook/LogInModel.java
@@ -0,0 +1,32 @@
+package com.lions.cookbook;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogInModel {
+    private FirebaseAuth mAuth;
+
+    public LogInModel(){
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public boolean signIn(String email, String password){
+        Task<AuthResult> signInIsSuccessful = mAuth.signInWithEmailAndPassword(email, password);
+        return signInIsSuccessful.isSuccessful();
+    }
+
+    //returns FirebaseUser if someone signed in, returns null otw
+    public FirebaseUser getCurrentUser(){
+        return mAuth.getCurrentUser();
+    }
+
+
+    public void signOut(){
+        mAuth.signOut();
+    }
+
+    public FirebaseAuth getmAuth() {
+        return mAuth;
+    }
+}
