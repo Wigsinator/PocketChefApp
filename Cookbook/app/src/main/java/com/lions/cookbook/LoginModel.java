41917a67a0c9a16d67af1fe2a1a9c9cab758a98f
Jennifer Huynh <jenniferr.huynh@mail.utoronto.ca>
2020-03-24 20:00:43 -0400

    [LION-63] Remove null object reference

--- Cookbook/app/src/main/java/com/lions/cookbook/LoginModel.java
+++ Cookbook/app/src/main/java/com/lions/cookbook/LoginModel.java
@@ -2,6 +2,9 @@ package com.lions.cookbook;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.AuthResult;
 import com.google.android.gms.tasks.Task;
@@ -9,15 +12,22 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginModel implements LoginContract.LoginModel{
     private FirebaseAuth mAuth;
+    private Boolean signedIn;
 
     public LoginModel(){
         mAuth = FirebaseAuth.getInstance();
+        signedIn = false;
     }
 
     public boolean signIn(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password);
-        Log.d("currUser", mAuth.getCurrentUser().getEmail());
-        return mAuth.getCurrentUser() != null && (mAuth.getCurrentUser().getEmail().equals(email));
+        OnCompleteListener signinListeneter = new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                signedIn = true;
+            }
+        };
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(signinListeneter);
+        return signedIn;
     }
 
     //returns FirebaseUser if someone signed in, returns null otw
