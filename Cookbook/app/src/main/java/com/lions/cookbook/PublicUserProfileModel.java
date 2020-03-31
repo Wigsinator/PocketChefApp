027c7e2a6fdb24c1a8a6ff90c40094648c3e1ecc
Jennifer Huynh <jenniferr.huynh@mail.utoronto.ca>
2020-03-31 19:55:45 -0400

    [LION-106] Adjust queries to match db structure

--- Cookbook/app/src/main/java/com/lions/cookbook/PublicUserProfileModel.java
+++ Cookbook/app/src/main/java/com/lions/cookbook/PublicUserProfileModel.java
@@ -8,10 +8,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PublicUserProfileModel implements PublicUserProfileContract.PublicUserProfileModel {
     FirebaseAuth mAuth;
@@ -43,19 +45,27 @@ public class PublicUserProfileModel implements PublicUserProfileContract.PublicU
         }
     }
 
+    public String extractID(String accountInfoStr){
+        return accountInfoStr.substring(accountInfoStr.indexOf('{')+1, accountInfoStr.indexOf('='));
+    }
+
     public void findFullname(){
         Query query = db.child("users").orderByChild("username").equalTo(this.username);
         ValueEventListener fullNameListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()){
-                    fullname = dataSnapshot.child("fullname").getValue(String.class);
+                    String accountInfoStr = dataSnapshot.getValue().toString();
+                    String uid = extractID(accountInfoStr);
+                    fullname = dataSnapshot.child(uid).child("fullname").getValue(String.class);
+                    if (fullname == null){
+                        Log.d("fnn", "full name was null");
+                    }
                     notifyAllObservers();
-                    if (fullname!= null) {
+                    if (fullname != null) {
                         Log.d("TEST", "got the correct FULL NAME in public profile MODEL:".concat(fullname));
                     }
                 }
-
             }
 
             @Override
@@ -72,9 +82,14 @@ public class PublicUserProfileModel implements PublicUserProfileContract.PublicU
         ValueEventListener recipeListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("inside OnDataChange", "inside recipe OnDataChange");
                 if (dataSnapshot.exists()){
-                    for (DataSnapshot recipeSnapshot : dataSnapshot.child("recipes").getChildren()){
-                        recipes.add(recipeSnapshot.getValue(String.class));
+                    String accountInfoStr = dataSnapshot.getValue().toString();
+                    String uid = extractID(accountInfoStr);
+                    for (DataSnapshot recipeSnapshot : dataSnapshot.child(uid).child("cookbook").getChildren()){
+                        String recipeName =  recipeSnapshot.getValue(String.class);
+                        recipes.add(recipeName);
+                        Log.d("recipe added", "a recipe was added");
                     }
                     notifyAllObservers();
 
